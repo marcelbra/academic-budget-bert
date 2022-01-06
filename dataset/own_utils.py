ff3ba905ffe1e31278db5d3d7826de3c1a180ec6
marcelbra <marcelbraasch@gmail.com>
2022-01-06 15:24:15 +0100

    Changed scripts

--- dataset/own_utils.py
+++ dataset/own_utils.py
@@ -6,32 +6,35 @@ def process_wiki_from_disk():
     print("Start process")
     print("Start loading data.")
     wiki = load_from_disk("/home/marcelbraasch/PycharmProjects/LM_pre_training/Wikipedia/raw")["train"]
-    tenth = int(len(wiki)/10)
+    #tenth = int(len(wiki)/10)
     print("End loading data.")
-    c = 0
+    #c = 0
     print("Start writing.")
-    with open("Wikipedia/wiki_for_24h_BERT_tenth.txt", mode="w") as ofile:
+    with open("Wikipedia/wiki_for_24h_BERT.txt", mode="w") as ofile:
         for article in tqdm(wiki):
             ofile.write(article["text"].replace('\n', ' ') + "\n\n")
-            c += 1
-            if c>=tenth: break
+            #c += 1
+            #if c>=tenth: break
     print("End writing.")
     print("End process")
+
+process_wiki_from_disk()
 
 def merge_shards():
     """
     Expects a (possibly recursively variably deep, in this case two) directory of
     training and test shards and renames them according to a counter.
     """
-    files1 = os.listdir("~/PycharmProjects/academic-budget-bert/dataset/data/Wikipedia/shards_1")
-    files2 = os.listdir("~/PycharmProjects/academic-budget-bert/dataset/data/Wikipedia/shards_2")
+    files1 = os.listdir("~/PycharmProjects/academic-budget-bert/dataset/data/Wikipedia/shards_1_copy")
+    files2 = os.listdir("~/PycharmProjects/academic-budget-bert/dataset/data/Wikipedia/shards_2_copy")
     training_counter, test_counter = 0,0
     for file in files1 + files2:
         if file.startswith("training"):
             os.rename(file, f"training{training_counter}.txt")
             training_counter += 1
         else:
-            os.rename(file, f"training{test_counter}.txt")
+            os.rename(file, f"test{test_counter}.txt")
             test_counter += 1
 
 merge_shards()
+
