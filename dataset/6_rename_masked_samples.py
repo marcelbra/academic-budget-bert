61de13be4d25ade4455ce9f9d5a528d25acbf819
marcelbra <marcelbraasch@gmail.com>
2022-04-22 18:43:35 +0100

    Fixed renaming

--- dataset/6_rename_masked_samples.py
+++ dataset/6_rename_masked_samples.py
@@ -2,17 +2,16 @@ import os
 import random
 
 path = "/home/marcelbraasch/PycharmProjects/academic-budget-bert/dataset/helper/Wikipedia/5_MaskedSamples/"
+#path = "/Users/marcelbraasch/Desktop/TestFiles/"
 files = os.listdir(path)
 random.shuffle(files)
 n = len(files)
 counter = 0
-test_counter = 0
 training_max = int(n*0.9)
 for file in files:
     if counter < training_max:
         new_name = f"train_shard_{counter}.hdf5"
     else:
-        new_name = f"test_shard_{test_counter}.hdf5"
-        test_counter += 1
-    os.rename(path + file, path + new_name)
+        new_name = f"test_shard_{counter}.hdf5"
     counter += 1
+    os.rename(path + file, path + new_name)
